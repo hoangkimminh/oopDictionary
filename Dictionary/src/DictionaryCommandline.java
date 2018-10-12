793f839d8d4919be93c0ef1237a9349c0474f552
hoangkimminh <41377837+hoangkimminh@users.noreply.github.com>
2018-10-12 21:48:34 +0700

    change

--- Dictionary/src/DictionaryCommandline.java
+++ Dictionary/src/DictionaryCommandline.java
@@ -1,54 +1,56 @@
 import java.util.Scanner;
-
-//This class run the commandline application
+//This class run Dictionary Commandline Application
 public class DictionaryCommandline {
 
-    //Property of Dictionary of DictionaryCommandline
+    //Property of DictionaryCommandline
     private DictionaryManagement dictionaryManagement = new DictionaryManagement();
 
-    //This method calls showAllWords method in dictionaryManagement
+    //This method calls method showAllWords from dictionaryManagement
     public void showAllWords() {
         dictionaryManagement.showAllWords();
     }
 
-    //This method contains two function: insert some new words and show all words in the dictionary
+    //This method contains 2 function: insert  words from commandline and show all words in dictionary
     public void dictionaryBasic() {
         dictionaryManagement.insertFromCommandLine();
         showAllWords();
     }
 
-    //This method contains 3 function: insert some new words from .txt file, show all words in the dictionary and look up a word.
+    //This method contains 3 function: insert words from .txt file, show all words and look up exact word in dictionary
     public void dictionaryAdvanced() {
         dictionaryManagement.insertFromFile();
         showAllWords();
         dictionaryManagement.dictionaryLookup();
     }
 
-    //This method calls dictionarySearcher in dictionaryManagement
+    //This method call method dictionarySearcher in dictionaryManagement
     public void dictionarySearcher() {
         dictionaryManagement.dictionarySearcher();
     }
 
-    //This is the main method runs commandline application
+    //Main function
     public static void main(String[] args) {
         DictionaryCommandline cmd = new DictionaryCommandline();
+        //Insert words from file
         cmd.dictionaryManagement.insertFromFile();
         Scanner scanner = new Scanner(System.in);
-        int command = 0;        //input a command and do similar function
+        int command = 0;
+        //List of functions in this application
         while (true) {
             switch (command) {
                 case 0: {
+                    //Show functions
                     System.out.println(new String(new char[50]).replace("\0", "\n\r"));
-                    System.out.println("\nDictionary: English - Vietnamese");
-                    System.out.println("1. Show all words");
-                    System.out.println("2. Find word");
-                    System.out.println("3. Search");
-                    System.out.println("4. Add word");
-                    System.out.println("5. Change word meaning");
-                    System.out.println("6. Delete word");
-                    System.out.println("7. Export to file");
-                    System.out.println("Input letter to exit program");
-                    System.out.print("Your command: ");
+                    System.out.print("\nDictionary: English - Vietnamese" +
+                                     "\n1. Show all words" +
+                                     "\n2. Find word" +
+                                     "\n3. Search" +
+                                     "\n4. Add word" +
+                                     "\n5. Change word meaning" +
+                                     "\n6. Delete word" +
+                                     "\n7. Export to file" +
+                                     "\nInput letter to exit program" +
+                                     "\nYour command: ");
                     if (scanner.hasNextInt()) {
                         command = scanner.nextInt();
                         System.out.println();
@@ -61,54 +63,61 @@ public class DictionaryCommandline {
                 case 1: {
                     //Show all words
                     cmd.showAllWords();
+                    System.out.print("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
                     break;
                 }
                 case 2: {
-                    //Look up a word
+                    //Lookup an exact word in dicitonary
                     cmd.dictionaryManagement.dictionaryLookup();
+                    System.out.println("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
                     break;
                 }
                 case 3: {
-                    //Search list of words contains a string
+                    //Search a string
                     cmd.dictionarySearcher();
+                    System.out.println("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
                     break;
                 }
                 case 4: {
-                    //Add a word to the dictionary
+                    //Add a word to dictionary
                     cmd.dictionaryManagement.addWord();
+                    System.out.println("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
                     break;
                 }
                 case 5: {
-                    //Change a word'meaning
+                    //Change word's meaning
                     cmd.dictionaryManagement.changeWordMeaning();
+                    System.out.println("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
                     break;
                 }
                 case 6: {
-                    //Delete a word from dictionary
+                    //Delete a word
                     cmd.dictionaryManagement.deleteWord();
+                    System.out.println("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
                     break;
                 }
                 case 7: {
-                    //Export the dictionary to a .txt file
+                    //Export the dictionary to .txt file
                     cmd.dictionaryManagement.dictionaryExportToFile();
+                    System.out.println("Press enter to continue");
                     scanner.nextLine();
                     scanner.nextLine();
                     command = 0;
